7188c9d1329d8b0bf46133d15bbb24eb14faaa05
sebastien <sebastienlorandel@gmail.com>
2018-10-16 22:12:43 -0400

    scroll lesson

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-       // if($(document).scrollTop() > 150)
-       //     $('.lowBox').addClass('show')
-
+       
+        ShowBoiteIfScrollPast150Px();
         
     });
     
 });
 
+function ShowBoiteIfScrollPast150Px()
+{
+    if($(document).scrollTop() > 150)
+           $('.boite').addClass('show')
+}
